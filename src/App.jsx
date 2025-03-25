8d299694133fc4023c1028325b971491f86077bf
privat <damiansagan@MacBookAir.fritz.box>
2025-03-25 14:47:50 +0100

    addNavbarfromDaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>MyPokemnonStuff</h1>
+    <div className="navbar bg-[#941f8a] text-neutral-content">
+  <button className="btn btn-ghost text-xl">My Pokemons</button>
+</div>
+
+<div className="navbar bg-[#1f9473]">
+  <button className="btn btn-ghost text-xl">Favorite Pokemons</button>
+</div>
+
+<div className="navbar bg-[#e7f20f] text-black">
+  <button className="btn btn-ghost text-xl">Something Else with Pokemon</button>
+</div>
+    
+
     </>
   )
 }
